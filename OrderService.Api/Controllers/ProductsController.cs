0673e583dfc2c7b046c32539e1dc80410283f25e
Fabian <correamarques@gmail.com>
2026-04-03 15:36:05 -0300

    first commit

---
+++ OrderService.Api/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
